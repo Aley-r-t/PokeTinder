2c9d66682d97c0f44da4cf68cfcf3f1206d0da1d
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-11 22:50:57 -0500

    more change

--- app/src/main/java/com/contreras/rodrigo/poketinder/MainActivity.kt
+++ app/src/main/java/com/contreras/rodrigo/poketinder/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.contreras.rodrigo.poketinder
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.contreras.rodrigo.poketinder.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private var listPokemon: List<PokemonResponse> = emptyList()
     private val adapter by lazy { PokemonAdapter(listPokemon) }
     private lateinit var binding : ActivityMainBinding
+
+    private val viewModel by lazy { MainViewModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,41 +19,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rvTinderPokemon.adapter = adapter
-        getAllPokemons()
+        observeValues()
     }
 
+    private fun observeValues() {
+        viewModel.isLoading.observe(this) { isLoading ->
+            binding.progressBar.isVisible = isLoading
+        }
 
-    private fun getAllPokemons() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val request = getRetrofit().create(PokemonApi::class.java).getPokemons()
-            if (request.isSuccessful) {
-                request.body()?.let {
-                    runOnUiThread {
-                        adapter.list = it.results
-                        adapter.notifyDataSetChanged()
-                    }
-                }
-            }
+        viewModel.pokemonList.observe(this) { pokemonList ->
+            adapter.list = pokemonList
+            adapter.notifyDataSetChanged()
+        }
+
+        viewModel.errorApi.observe(this) { errorMessage ->
+            showMessage(errorMessage)
         }
     }
 
-    private fun getRetrofit(): Retrofit {
-        val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
-            val newRequest = chain.request().newBuilder()
-                .addHeader("Content-Type", "application/json")
-                .addHeader("NAME-HEADER", "VALUE-HEADER")
-                .build()
-            chain.proceed(newRequest)
-        }
-
-        val client = httpClient.build()
-
-        return Retrofit.Builder()
-            .baseUrl("https://pokeapi.co")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
+    private fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-
 
 }
