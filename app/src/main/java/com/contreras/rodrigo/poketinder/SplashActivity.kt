b937c84ff313933fe06e3969a7661c041e056739
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-11 22:39:08 -0500

    Splash Create

--- app/src/main/java/com/contreras/rodrigo/poketinder/SplashActivity.kt
+++ app/src/main/java/com/contreras/rodrigo/poketinder/SplashActivity.kt
@@ -1,20 +1,49 @@
 package com.contreras.rodrigo.poketinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.contreras.rodrigo.poketinder.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setContentView(binding.root)
+
+        showAnimationLogo()
+        runPostDelayed()
+
+    }
+
+    private fun goMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+
+    private fun runPostDelayed() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            goMainActivity()
+        }, 4000)
+    }
+
+    private fun showAnimationLogo() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
